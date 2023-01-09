0c75c510146fcffc88fcfa786c20fe94867a2c54
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-01-09 13:43:18 +0500

    09

--- String09.py
+++ String09.py
@@ -6,5 +6,5 @@ def main(s):
     Returns:
         int: answer
     """
-    return s.count('g')
-print(main('google is a search engine'))
+    return s.count('a')
+print(main('google is a search engine')) 
